865a57e5e3d74b7162acb75350689e8799726094
Jibitesh_Saha <jibiteshsaha@gmail.com>
2025-08-22 18:44:36 +1000

    Completed second draft of aws_json_1_1.py

--- processors/aws_json_1_1.py
+++ processors/aws_json_1_1.py
@@ -2,9 +2,11 @@
 
 import json, yaml
 from pathlib import Path
+import sys
 from processors.shared_functions import (
     LiteralStr,
     literal_str_representer,
+    html_to_md,
     init_openapi_spec,
     add_info,
     add_servers,
@@ -49,10 +51,17 @@ def process(model_entry):
 
     shapes = model_data.get("shapes", model_data)
 
+    shapes_dict = {
+        "service": [],
+        "operation": []
+    }
+
     for shape_name, shape in shapes.items():
         if shape.get("type") == "service": 
             add_info(openapi_spec, shape)
             add_servers(openapi_spec, file_name, shape)
+            shape["my_name"] = shape_name
+            shapes_dict["service"].append(shape)
         elif shape.get("type") == "string":
             add_component_schema_string(openapi_spec, shape_name, shape)
         elif shape.get("type") == "boolean":
@@ -83,7 +92,22 @@ def process(model_entry):
             add_component_schema_structure(openapi_spec, shape_name, shape)
         elif shape.get("type") == "operation":
             add_operation(openapi_spec, shape_name, shape, shapes)
+            shape["my_name"] = shape_name
+            shapes_dict["operation"].append(shape)
 
+    # process the service to get the paths
+    service_name2 = model_entry['servicename'].split('#')[1]
+    
+    # Sort the operations, we will need them to be in alphabetic order for creating paths
+    shapes_dict["operation"].sort(key=lambda x: x["my_name"])
+
+    # Setup the "paths" attribute
+    openapi_spec["paths"] = {}
+
+    # create the path
+    for operation in shapes_dict["operation"]:
+        key_string = "/#X-Amz-Target=" + service_name2 + "." + operation["my_name"].split('#')[1]
+        openapi_spec["paths"][key_string] = create_path(operation, service_name2)
 
     # Write output YAML
     outdir = Path("testdir")
@@ -92,8 +116,63 @@ def process(model_entry):
     with open(outfile, "w", encoding="utf-8") as f:
         yaml.dump(openapi_spec, f, sort_keys=False, allow_unicode=True)
 
+def create_path(operation, service_name2):
+    result = {}
+    result["post"] = {}
+    result_post = result["post"]
+    
+    result_post["operationId"] = operation["my_name"].split('#')[1]
+    result_post["description"] = LiteralStr(html_to_md(operation["traits"].get("smithy.api#documentation", "")))
+    
+    result_post["requestBody"] = {}
+    result_request_body = result_post["requestBody"]
+    result_request_body["required"] = True
+    result_request_body["content"] = {}
+    result_request_body["content"]["application/json"] = {}
+    result_request_body["content"]["application/json"]["schema"] = {}    
+    result_request_body["content"]["application/json"]["schema"]["$ref"] = "#/components/schemas/" + operation["input"]["target"].split("#")[1]
 
+    result_post["parameters"] = {}
+    result_parameters_inside = result_post["parameters"]
+    result_parameters_inside["name"] = "X-Amz-Target"
+    result_parameters_inside["in"] = "header"
+    result_parameters_inside["required"] = True
+    result_parameters_inside["schema"] = {}
+    result_parameters_inside["schema"]["type"] = "string"
+    result_parameters_inside["schema"]["enum"] = [service_name2 + "." + operation["my_name"].split('#')[1]]
 
-# def process(model_entry):
-#     service_name = model_entry['servicename'].split('#')[0].split('com.amazonaws.')[1]
-#     print(f"processing {service_name} with protocol {model_entry['protocol']}")
+    # Static Information
+    result["parameters"] = []
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-Content-Sha256'})
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-Date'})
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-Algorithm'})
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-Credential'})
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-Security-Token'})
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-Signature'})
+    result["parameters"].append({ '$ref': '#/components/parameters/X-Amz-SignedHeaders'})
+
+    result_post["responses"] = {}
+    result_responses = result_post["responses"]
+    # 200 response
+    result_responses["200"] = {}
+    result_responses["200"]["description"] = "Success"
+    result_responses["200"]["content"] = {}
+    result_responses["200"]["content"]["application/json"] = {}
+    result_responses["200"]["content"]["application/json"]["schema"] = {"$ref": ('#/components/schemas/' + operation["output"]["target"].split('#')[1])}
+
+    error_code = 480
+
+    if operation.get("errors", False) is not False:
+        for error in operation["errors"]:
+            error_string = str(error_code)
+            error_name = error["target"].split('#')[1]
+
+            result_responses[error_string] = {}
+            result_responses[error_string]["description"] = error_name
+            result_responses[error_string]["content"] = {}
+            result_responses[error_string]["content"]["application/json"] = {}
+            result_responses[error_string]["content"]["application/json"]["schema"] = {"$ref": ('#/components/schemas/' + error_name)}
+
+            error_code += 1
+
+    return result
